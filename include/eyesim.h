3d52eab5719db1082ef8201b31b272403e4e6c9c
super148666 <zc21435393@gmail.com>
2017-10-16 16:32:38 +0800

    eyesim class header

---
+++ include/eyesim.h
@@ -0,0 +1,51 @@
+//
+// Created by chaoz on 16/10/17.
+//
+
+#ifndef ROBOT_TOUR_EYESIM_H
+#define ROBOT_TOUR_EYESIM_H
+#include <Aria.h>
+#include <iostream>
+
+#define LASER_LEFT 181
+#define LASER_RIGHT 0
+#define LASER_FRONT 90
+
+using namespace std;
+
+class eyesim {
+private:
+    /* data */
+
+    ArRobot robot;
+    ArSick laser;
+
+public:
+    eyesim(int*,char**);
+    int SIMLaserScan(int*);
+    int VWSetSpeed(int, int);
+
+    int VWGetSpeed(int *, int *);
+
+    int VWSetPosition(int, int, int);
+
+    int VWGetPosition(int *, int *, int *);
+
+    int VWStraight(int, int);
+
+    int VWTurn(int, int);
+
+    int VWCurve(int, int, int);
+
+    int VWDrive(int, int, int);
+
+    int VWRemain(void);
+
+    int VWDone(void);
+
+    int VWWait(void);
+
+    int VWStalled(void);
+};
+
+#endif //ROBOT_TOUR_EYESIM_H
