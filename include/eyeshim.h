fbfbaf10a68153e059507c35d61039518c6a5ffd
super148666 <zc21435393@gmail.com>
2017-10-16 16:31:24 +0800

    eyesim class source code

--- include/eyeshim.h
+++ include/eyeshim.h
@@ -1,62 +1,101 @@
 //Aria Header
 
-#ifndef LIBARIA
+#ifndef EYESIM_H
 #include "Aria.h"
-#define LIBARIA
-#endif
 
-class XArRobot: public ArRobot{
+class XArRobot {
 private:
-  /* data */
+    /* data */
+
+    ArRobot robot;
+    ArSick laser;
 
 public:
-  int VWSetSpeed(int, int);
-  int VWGetSpeed(int*, int*);
-  int VWSetPosition(int, int, int);
-  int VWGetPosition(int*, int*, int*);
-  int VWStraight(int, int);
-  int VWTurn(int, int);
-  int VWCurve(int, int, int);
-  int VWDrive(int, int, int);
-  int VWRemain(void);
-  int VWDone(void);
-  int VWWait(void);
-  int VWStalled(void);
+    int SIMLaserScan(int* scan);
+    int VWSetSpeed(int, int);
+
+    int VWGetSpeed(int *, int *);
+
+    int VWSetPosition(int, int, int);
+
+    int VWGetPosition(int *, int *, int *);
+
+    int VWStraight(int, int);
+
+    int VWTurn(int, int);
+
+    int VWCurve(int, int, int);
+
+    int VWDrive(int, int, int);
+
+    int VWRemain(void);
+
+    int VWDone(void);
+
+    int VWWait(void);
+
+    int VWStalled(void);
 };
 
-int XArRobot::VWSetSpeed(int linSpeed, int angSpeed){
-  return(0);
+int XArRobot::VWSetSpeed(int linSpeed, int angSpeed) {
+    return (0);
 }
-int XArRobot::VWGetSpeed(int *linSspeed, int *angSpeed){
-  return(0);
+
+int XArRobot::VWGetSpeed(int *linSspeed, int *angSpeed) {
+    return (0);
 }
-int XArRobot::VWSetPosition(int x, int y, int phi){
-  return(0);
+
+int XArRobot::VWSetPosition(int x, int y, int phi) {
+    return (0);
 }
-int XArRobot::VWGetPosition(int *x, int *y, int *phi){
-  return(0);
+
+int XArRobot::VWGetPosition(int *x, int *y, int *phi) {
+    return (0);
 }
-int XArRobot::VWStraight(int dist, int lin_speed){
-  return(0);
+
+int XArRobot::VWStraight(int dist, int lin_speed) {
+    return (0);
 }
-int XArRobot::VWTurn(int angle, int ang_speed){
-  return(0);
+
+int XArRobot::VWTurn(int angle, int ang_speed) {
+    return (0);
 }
-int XArRobot::VWCurve(int dist, int angle, int lin_speed){
-  return(0);
+
+int XArRobot::VWCurve(int dist, int angle, int lin_speed) {
+    return (0);
 }
-int XArRobot::VWDrive(int dx, int dy, int lin_speed){
-  return(0);
+
+int XArRobot::VWDrive(int dx, int dy, int lin_speed) {
+    return (0);
 }
-int XArRobot::VWRemain(void){
-  return(0);
+
+int XArRobot::VWRemain(void) {
+    return (0);
 }
-int XArRobot::VWDone(void){
-  return(0);
+
+int XArRobot::VWDone(void) {
+    return (0);
 }
-int XArRobot::VWWait(void){
-  return(0);
+
+int XArRobot::VWWait(void) {
+    return (0);
 }
-int XArRobot::VWStalled(void){
-  return(0);
+
+int XArRobot::VWStalled(void) {
+    return (0);
 }
+
+int XArRobot::SIMLaserScan(int *scan) {
+    const list<ArSensorReading *> *readingsList;
+    list<ArSensorReading *>::const_iterator it;
+    int i = -1;
+    readingsList = laser->getRawReadings();
+    for (it = readingsList->begin(); it != readingsList->end(); it++) {
+        i++;
+        g_scan[i] = (*it)->getRange();
+    }
+    return 0;
+}
+
+#define EYESIM_H
+#endif
--- src/tourGuide.cpp
+++ src/tourGuide.cpp
@@ -1,6 +1,5 @@
 //Compile with g++ -Wall -o tourGuide -lAria -ldl `pkg-config --cflags --libs opencv` -lpthread -L /usr/local/Aria/lib -I/usr/local/Aria/include tourGuide.cpp
-
-#include "Aria.h"
+#include "../include/eyesim.h"
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/video.hpp>
@@ -8,13 +7,11 @@
 
 /* defines */
 #define REQUIRED_ARGUMENTS 1
+#define IMAGE_SCALE_FACTOR 10
 #define WINDOW_SIZE 300 // window size of image display
 #define SUBTRACT_HISTORY 50 // compared histroy of image in subtractor
-#define SUBTRACT_THRESHOLD 16 // threshold value for subtractor
-#define ROBOT_ENABLE 0  // set as 1 to turn on g_robot connection
-#define LASER_LEFT 180
-#define LASER_RIGHT 0
-#define LASER_FRONT 90
+#define SUBTRACT_THRESHOLD 3 // threshold value for subtractor
+
 
 /* Namespace */
 using namespace std;
@@ -23,42 +20,45 @@ using namespace cv;
 /* Global Variables */
 ArRobot g_robot;
 ArSick g_laser;
+int g_scan[181];    // 0-right; 90-front; 181-left;
 
 /* Declare Function */
 
 void printUsage();
 
-Ptr<int> GetLaser(Ptr<ArSick> laser);
+void GetLaser(Ptr<ArSick> laser);
 
 class MotionDetector {
 public:
     MotionDetector(int history = 500, double varThreshold = 16,
-                        bool detectShadows = true) {
+                   bool detectShadows = true) {
         bgSubtractor = createBackgroundSubtractorMOG2(history, varThreshold, detectShadows);
     }
 
     void imShowForeground(int windowSizeX = 0, int windowSizeY = 0) {
 
-        if((windowSizeX==0)&(windowSizeY==0)){
+        if ((windowSizeX == 0) & (windowSizeY == 0)) {
             namedWindow("foreground");
-        }else {
+        } else {
             namedWindow("foreground", WINDOW_NORMAL);
         }
 
         resizeWindow("foreground", windowSizeX, windowSizeY);
         imshow("foreground", foreground);
     }
+
     void imShowBackground(int windowSizeX = 0, int windowSizeY = 0) {
 
-        if((windowSizeX==0)&(windowSizeY==0)){
+        if ((windowSizeX == 0) & (windowSizeY == 0)) {
             namedWindow("background");
-        }else {
+        } else {
             namedWindow("background", WINDOW_NORMAL);
         }
 
         resizeWindow("background", windowSizeX, windowSizeY);
         imshow("background", background);
     }
+
     Mat Detect(Mat frame) {
         this->bgSubtractor->apply(frame, this->foreground);
         this->bgSubtractor->getBackgroundImage(this->background);
@@ -86,56 +86,38 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    if (ROBOT_ENABLE) {
-        /* initialisation of lbraries */
-        //  Aria
-        Aria::init();
-        g_robot.addRangeDevice(&g_laser);
-        ArArgumentParser parser(&argc, argv); //inst argument parser
-        ArSimpleConnector connector(&parser);           //inst connector
-        parser.loadDefaultArguments();
+    eyesim Pioneer(&argc, argv);
 
-        /* Connection to robot */
-        if (!connector.parseArgs()) {
-            cout << "Unknown settings\n";
-            Aria::exit(0);
-            exit(EXIT_FAILURE);
-        }
-        if (!connector.connectRobot(&g_robot)) {
-            cout << "Unable to connect\n";
-            Aria::exit(0);
-            exit(EXIT_FAILURE);
-        }
-        g_robot.runAsync(true);
-        g_laser.runAsync();
-        if (!connector.connectLaser(&g_laser)) {
-            cout << "Can't connect to g_laser\n";
-            Aria::exit(0);
-            exit(EXIT_FAILURE);
-        }
-        g_laser.asyncConnect();
-
-        g_robot.lock();
-        g_robot.comInt(ArCommands::ENABLE, 1);
-        g_robot.unlock();
-    }
 
     //  OpenCV
     VideoCapture cap;
-    if (!cap.open(0)) {
+    if (!cap.open(1)) {
         cout << "Unable to open webcam\n";
         exit(EXIT_FAILURE);
     }
     /* video thread */
     Mat frame;
-    MotionDetector motionDetector = MotionDetector(SUBTRACT_HISTORY,SUBTRACT_THRESHOLD);
+    MotionDetector motionDetector = MotionDetector(SUBTRACT_HISTORY, SUBTRACT_THRESHOLD);
+
+    int scan[181];
+    Pioneer.SIMLaserScan(scan);
+    for (int i = 0; i < 181; i++) {
+        cout << " list: " << i << " dist: " << scan[i] << endl;
+    }
+
+    cap >> frame;
+//    Size frameSize(frame.cols / IMAGE_SCALE_FACTOR, frame.rows / IMAGE_SCALE_FACTOR);
+    Size frameSize(40,30);
     namedWindow("webcam", WINDOW_NORMAL);
-    resizeWindow("webcam", WINDOW_SIZE, WINDOW_SIZE);
+    resizeWindow("webcam", frame.cols, frame.rows);
+//    Size frameSize(WINDOW_SIZE,WINDOW_SIZE);
 
     while (waitKey(10) != 27) {
 //      system("play -q sound-impressed.wav");
         cap >> frame;
         if (frame.empty()) break; // end of video stream
+        resize(frame, frame, frameSize);
+//        resize(frame,frame,)
         frame = motionDetector.Detect(frame);
         imshow("webcam", frame);
     }
@@ -157,9 +139,13 @@ void printUsage() {
     cout << endl;
 }
 
-Ptr<int> GetLaser(Ptr<ArSick> laser) {
-    const list<ArSensorReading*>* readingsList;
-    list<ArSensorReading*>::const_iterator it;
-
-
+void GetLaser(Ptr<ArSick> laser) {
+    const list<ArSensorReading *> *readingsList;
+    list<ArSensorReading *>::const_iterator it;
+    int i = -1;
+    readingsList = laser->getRawReadings();
+    for (it = readingsList->begin(); it != readingsList->end(); it++) {
+        i++;
+        g_scan[i] = (*it)->getRange();
+    }
 }
